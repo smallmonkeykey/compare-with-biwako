082849c1a545893b4ebe1fe7ac851b56bd6bc109
smallmonkeykey <sarinitakuoo@gmail.com>
2025-05-16 15:25:21 +0900

    一部の区のデータを仮でいれる

---
+++ data/tokyo-23-wards.js
@@ -0,0 +1,14 @@
+export const WARDS = [
+  {
+    name: "shibuya",
+    jp: "渋谷区",
+    aliases: ["渋谷区", "渋谷", "shibuya", "shibuya-ku"],
+    area: 15.11,
+  },
+  {
+    id: "shinjuku",
+    jp: "新宿区",
+    aliases: ["新宿区", "新宿", "shinjuku", "shinjuku-ku"],
+    area: 18.23,
+  },
+];
